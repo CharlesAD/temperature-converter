2cf73e952490ea36900c1d07425c515730205920
CharlesAD <c.acquahdavis@gamil.com>
2022-08-18 15:36:58 +0100

    Log values from controlled inputs

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import TempInput from "./components/Input/Input";
 
 function App() {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <>
       <h1 className="text-center">Temperature Converter</h1>
       <form className="mx-auto mt-8 flex w-1/3 justify-between">
-        <TempInput label="Celsius" />
-        <TempInput label="Fahrenheit" />
+        <TempInput label="Celsius" value={100} changeHandler={handleChange} />
+        <TempInput label="Fahrenheit" value={50} changeHandler={handleChange} />
       </form>
     </>
   );
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
 
-export default function TempInput({ label }) {
+export default function TempInput({ label, value, changeHandler }) {
   return (
     <div>
-      <label htmlFor={label.toLowerCase}>{label}</label>
+      <label htmlFor={label.toLowerCase()}>{label}</label>
       <input
         type="number"
-        id={label.toLowerCase}
+        id={label.toLowerCase()}
         className="w-36 rounded border"
+        value={value}
+        onChange={changeHandler}
       />
     </div>
   );
@@ -15,4 +17,6 @@ export default function TempInput({ label }) {
 
 TempInput.propTypes = {
   label: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  changeHandler: PropTypes.func.isRequired,
 };
